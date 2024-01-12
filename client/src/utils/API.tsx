ab735ce91bdf27181f6ab9c1ea7a9d6024445c3d
Bekam <mspbekan@gmail.com>
2024-01-11 18:01:12 -0600

    code added to the client-side api

--- client/src/utils/API.tsx
+++ client/src/utils/API.tsx
@@ -1,45 +1,132 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
-export interface MainDriverApi {
-  createMainDriver: (data:any) => Promise<string>;
-  getAllMainDriver: (data: any)=>Promise<string>
+interface MainDriverApi {
+  createMainDriver: (data: any) => Promise<string>;
+  getAllMainDriver: () => Promise<string[] | null>;
+  getAllDriverBySchool: () => Promise<string[] | null>;
+  getSingleDriver: () => Promise<string | null>;
+  getOnlySingleDriverProfile: () => Promise<string | null>;
+  updateDriver: (data: any) => Promise<string>;
 }
 
 const MainDriverApi: MainDriverApi = {
   createMainDriver: async (data) => {
     try {
-      const response = await axios
-        .post(`${API_BASE_URL}/driver`, data, {
-          headers: { "Content-Type": "application/json" },
-          // data: JSON.stringify(data)
-        })
-       return response.data;
-      // return response.data
+      const response = await axios.post(`${API_BASE_URL}/driver`, data, {
+        headers: { "Content-Type": "application/json" },
+      });
+      return response.data;
     } catch (error) {
       console.error("Server error occured", error);
-      throw error
+      throw error;
     }
   },
 
-getAllMainDriver:async (req: Request, res: Response) => {
+  getAllMainDriver: async () => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/driver/`, {
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        if(!response){
- return res.status(400).json({message: "Data requested not found. Please try again."})
+      const response: AxiosResponse<string[]> = await axios.get(
+        `${API_BASE_URL}/driver/`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-        return response.data
-    } catch( error) {
-        console.error("Server error occured.", error)
-        throw error
+      );
+      if (!response || response.status !== 200) {
+        console.error("Data requested not found. Please try again.");
+        return null;
+      }
+      return response.data;
+    } catch (error) {
+      console.error("Server error occured.", error);
+      throw error;
     }
-}
+  },
 
+  getSingleDriver: async () => {
+    try {
+      const response: AxiosResponse<string> = await axios.get(
+        `${API_BASE_URL}/driver/driverFirstName`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!response || response.status !== 200) {
+        console.error("Data requested not found. Please try again.");
+        return null;
+      }
+      return response.data;
+    } catch (error) {
+      console.error("Server error occured.", error);
+      throw error;
+    }
+  },
 
+  getOnlySingleDriverProfile: async () => {
+    try {
+      const response: AxiosResponse<string> = await axios.get(
+        `${API_BASE_URL}/driver/profile`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!response || response.status !== 200) {
+        console.error("Data requested not found. Please try again.");
+        return null;
+      }
+      return response.data;
+    } catch (error) {
+      console.error("Server error occured.", error);
+      throw error;
+    }
+  },
+  getAllDriverBySchool: async () => {
+    try {
+      const response: AxiosResponse<string[]> = await axios.get(
+        `${API_BASE_URL}/driver/tierAnchor_school`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!response || response.status !== 200) {
+        console.error("Data requested not found. Please try again.");
+        return null;
+      }
+      return response.data;
+    } catch (error) {
+      console.error("Server error occured.", error);
+      throw error;
+    }
+  },
 
-
+  updateDriver: async (data:any) => {
+    try {
+      const response: AxiosResponse<string> = await axios.put(
+        `${API_BASE_URL}/driver/driverFirstName`,
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!response || response.status !== 200) {
+        console.error("Data requested not found. Please try again.");
+        return null;
+      }
+      return response.data;
+    } catch (error) {
+      console.error("Server error occured.", error);
+      throw error;
+    }
+  },
 };
+
+export { MainDriverApi };
